110b63e2f11db2940daec6d3517c23e885ecd9bf
xcesco <xcesco@gmail.com>
2020-11-23 00:32:19 +0100

    @wip build problem

---
+++ CONF.md
@@ -0,0 +1,3 @@
+# Projection configuration
+The _integration test_ environment is done with test profile enabled with default properties put in
+ `test\resource\application-default.properties` file.
--- src/main/java/com/abubusoft/powertrainer/backend/web/EnvironmentController.java
+++ src/main/java/com/abubusoft/powertrainer/backend/web/EnvironmentController.java
@@ -1,17 +1,14 @@
 package com.abubusoft.powertrainer.backend.web;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 @RestController
 @RequestMapping(value = WebPathConstants.API_ENTRYPOINT + "/env")
 public class EnvironmentController {
-  @GetMapping
-  public Map<String, String> env() {
-    return System.getenv();
-  }
+//  @GetMapping
+//  public Map<String, String> env() {
+//    return System.getenv();
+//  }
 
 }
