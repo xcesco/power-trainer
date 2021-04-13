2ac35d6fd9040e09256345518ac267b09f2f141d
xcesco <xcesco@gmail.com>
2021-04-13 16:30:29 +0200

    power-trainer-16 put my exercise on fake-data

--- REFERENCE.md
+++ REFERENCE.md
@@ -4,3 +4,4 @@ https://www.npmjs.com/package/ngx-markdown
 https://github.com/KillerCodeMonkey/ngx-quill
 
 https://blog.angular-university.io/angular-custom-form-controls/
+https://www.npmjs.com/package/ng-expandable-area
--- src/main/webapp/app/entities/exercise/exercise.module.ts
+++ src/main/webapp/app/entities/exercise/exercise.module.ts
@@ -8,9 +8,10 @@ import { ExerciseDeleteDialogComponent } from './delete/exercise-delete-dialog.c
 import { ExerciseRoutingModule } from './route/exercise-routing.module';
 import { MarkdownModule } from 'ngx-markdown';
 import { QuillModule } from 'ngx-quill';
+import { ExpandableAreaModule } from 'ng-expandable-area';
 
 @NgModule({
-  imports: [SharedModule, ExerciseRoutingModule, MarkdownModule.forRoot(), QuillModule.forRoot()],
+  imports: [SharedModule, ExerciseRoutingModule, MarkdownModule.forRoot(), QuillModule.forRoot(), ExpandableAreaModule],
   declarations: [ExerciseComponent, ExerciseDetailComponent, ExerciseUpdateComponent, ExerciseDeleteDialogComponent],
   entryComponents: [ExerciseDeleteDialogComponent],
 })
